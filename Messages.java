5df72c6c2ae97908b281b6fd1ef2c2a76439c948
Nandipha-Mnaba <nandiphamnaba11@gmail.com>
2026-05-19 11:12:51 +0200

    added create message hash and fixed spelling errors

--- Messages.java
+++ Messages.java
@@ -9,8 +9,7 @@ import java.util.Random;
  */
 public class Messages {
     private String messageID;
-    private int    messageNumber;
-    private String recipient ;
+    private int messageNumber;
     private String messageText;
     private String messageHash;
     
@@ -22,18 +21,6 @@ public class Messages {
         this.messageID     = generateMessageID();
         this.messageHash   =createMessageHash();
     }
-    private String generateMessageID(){
-        Random rand = new Random();
-        long id = (long)(rand.nextDouble()* 9_000_000_000L)+ 1_000_000_000L;
-        return String.valueOf(id);
-                
-    }
-    public boolean checkMessage(){
-        return messageID.length() < 10;
-    } 
-    public String checkRecipientell(){
-        if (!recipient.startsWith("=")|| recipient.length()> 10){
-            return "Cell phone number is incorrectly formatted or does not contain an international code. Please correct the number and try again.";
-        }
-    }
+    
+    
 }
--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -35,5 +35,15 @@ public class Messages {
         if (!recipient.startsWith("=")|| recipient.length()> 10){
             return "Cell phone number is incorrectly formatted or does not contain an international code. Please correct the number and try again.";
         }
+        return "Cell phone number successfully captured.";
     }
+    //method 3 creatye a message hash 
+    // it build s (first 2 didigits of ID) :( messageNumber : FIRSTWORDLASTWORD)
+   // E.G 00:HITOGNIGHT
+    public String createMessageHash(){
+        String idPrefix  = messageID.substring(0,2);
+        String [] words  =messageText.trim().split("\\s+");
+        String firstWord =words[0];
+        String lastword  +words[words.length - 1].replaceAll("[^a-zA-Z0-9","");
+        return (idPrefix +":" + messageNumber +":" + firstWord +lastword) > toUpperCase ();
 }
